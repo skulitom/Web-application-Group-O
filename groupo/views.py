5fc08fde265c624a1176882ee6d9f6a7e12cc14b
Varun Mathur <vkmathur97@yahoo.co.uk>
2016-11-01 20:58:02 +0000

    update homepage view to retrieve user citations db

--- groupo/views.py
+++ groupo/views.py
@@ -2,4 +2,13 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    current_user = request.user
+    if current_user.is_authenicated():
+        citation_list = Citation.objects.filter(owner=current_user)
+        template = loader.get_template('polls/index.html')
+        context = {
+            'citation_list': citation_list,
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        return HttpResponseRedirect("/login/")

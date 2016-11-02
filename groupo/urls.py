d46fa0cbb6bb3f982295798b404dbc78010fd8ac
Varun Mathur <vkmathur97@yahoo.co.uk>
2016-11-02 07:09:10 +0000

    register url route

--- groupo/urls.py
+++ groupo/urls.py
@@ -29,4 +29,10 @@ urlpatterns = patterns('',
   url(r'^logout/$',
     'django.contrib.auth.views.logout', {'next_page': '/login/'}
 ),
+  url('^register/', CreateView.as_view(
+            template_name='register.html',
+            form_class=UserCreationForm,
+            success_url='/'
+    )),
+    url('^accounts/', include('django.contrib.auth.urls')),
 )

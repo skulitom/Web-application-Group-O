9c4d1625854379556d1af29a7a56aab34c268ce1
Sidharth Sikka <sidharthsikka@users.noreply.github.com>
2016-11-03 15:48:31 +0000

    Update prod.py

--- groupo/settings/prod.py
+++ groupo/settings/prod.py
@@ -1,1 +1,20 @@
 from groupo.settings.common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'Lonely_cat',
+        'USER' : 'Master',
+        'PASSWORD' : 'masterbeater',
+       'HOST' : 'groupo-db1.csbt6xxcttnx.eu-west-1.rds.amazonaws.com',
+       'PORT' : '5432',
+   }
+}

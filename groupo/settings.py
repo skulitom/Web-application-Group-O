84186212caad80ffc30814c1e505f5844ef660b3
varun <zcabvkm@ucl.ac.uk>
2016-11-03 00:12:56 +0000

    initial

--- groupo/settings.py
+++ groupo/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'groupo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+import urlparse
+db_url = urlparse.urlparse(os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL'))
+
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ['OPENSHIFT_APP_NAME'],
+    'USER': db_url.username,
+    'PASSWORD': db_url.password,
+    'HOST': db_url.hostname,
+    'PORT': db_url.port,
     }
 }
 

6f4dd6224fe6dac2742e18e5c5be6f25d3fb176b
Sidharth Sikka <sidharthsikka@users.noreply.github.com>
2016-11-03 15:43:14 +0000

    Update common.py

--- groupo/settings/common.py
+++ groupo/settings/common.py
@@ -46,6 +46,22 @@ LOGIN_REDIRECT_URL = '/citations/'
 
 WSGI_APPLICATION = 'groupo.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators

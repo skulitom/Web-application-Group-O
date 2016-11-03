d58001716ae7a097a7d74317cf86454fc43c1bbd
Artem <artemskulimovskiy@gmail.com>
2016-11-03 16:18:22 +0000

    Update project_conf.py

--- fabfile/project_conf.py
+++ fabfile/project_conf.py
@@ -54,10 +54,10 @@ fabconf['DOMAINS'] = "example.com www.example.com"
 fabconf['VIRTUALENV_DIR'] = "/home/%s/.virtualenvs" % fabconf['SERVER_USERNAME']
 
 # Email for the server admin
-fabconf['ADMIN_EMAIL'] = "admin@example.com"
+fabconf['ADMIN_EMAIL'] = "artemskulimovskiy@gmail.com"
 
 # Git username for the server
-fabconf['GIT_USERNAME'] = "EC2"
+fabconf['GIT_USERNAME'] = "skulitom"
 
 # Name of the private key file used for git deployments
 fabconf['GIT_DEPLOY_KEY_NAME'] = "git_rsa"
@@ -67,7 +67,7 @@ fabconf['GIT_DEPLOY_KEY_PATH'] = "%s/%s" % (
     fabconf['SSH_PATH'], fabconf['GIT_DEPLOY_KEY_NAME'])
 
 # The top-level domain name for your remote git service
-fabconf['GIT_HOST_DOMAIN'] = "bitbucket.org"
+fabconf['GIT_HOST_DOMAIN'] = "github.com"
 
 # Path to the repo of the application you want to install. The
 # REPO_USERNAME could be same as the GIT_USERNAME if the repo is under the
@@ -75,8 +75,8 @@ fabconf['GIT_HOST_DOMAIN'] = "bitbucket.org"
 # the projects owner. The REPO_NAME can contain slashes (eg
 # <project_name>/<repo_name> if you are using Bitbucket and the project is
 # within a project)
-fabconf['REPO_USERNAME'] = ''
-fabconf['REPO_NAME'] = ''
+fabconf['REPO_USERNAME'] = 'skulitom'
+fabconf['REPO_NAME'] = 'Web-application-Group-O'
 
 # Creates the ssh location of your remote repo from the above details
 fabconf['REPO_URL'] = "ssh://git@%s/%s/%s.git" % (
@@ -87,13 +87,13 @@ fabconf['ACTIVATE'] = "source /home/%s/.virtualenvs/%s/bin/activate" % (
     fabconf['SERVER_USERNAME'], fabconf['PROJECT_NAME'])
 
 # Name tag for your server instance on EC2
-fabconf['INSTANCE_NAME_TAG'] = "MyInstance"
+fabconf['INSTANCE_NAME_TAG'] = "skulitom3"
 
 # EC2 key. http://bit.ly/j5ImEZ
-fabconf['AWS_ACCESS_KEY'] = ''
+fabconf['AWS_ACCESS_KEY'] = 'AKIAIZ5A7GUKL7PIPRCQ'
 
 # EC2 secret. http://bit.ly/j5ImEZ
-fabconf['AWS_SECRET_KEY'] = ''
+fabconf['AWS_SECRET_KEY'] = '2Y8FOBSghf/PAZ6GGsD7/TLEXhxtYThvUp57C4EG'
 
 # EC2 region. http://amzn.to/12jBkm7
 ec2_region = 'ap-southeast-2'
@@ -102,13 +102,13 @@ ec2_region = 'ap-southeast-2'
 ec2_amis = ['ami-51821b6b']
 
 # Name of the keypair you use in EC2. http://bit.ly/ldw0HZ
-ec2_keypair = 'insert_keypair_name'
+ec2_keypair = 'skulitom'
 
 # Name of the security group. http://bit.ly/kl0Jyn
-ec2_secgroups = ['MySecurityGroup']
+ec2_secgroups = ['web-tier']
 
 # API Name of instance type. http://bit.ly/mkWvpn
 ec2_instancetype = 't2.micro'
 
 # Existing instances - add the public dns of your instances here when you have spawned them
-fabconf['EC2_INSTANCES'] = [""]
+fabconf['EC2_INSTANCES'] = ["ec2-52-213-61-39.eu-west-1.compute.amazonaws.com"]

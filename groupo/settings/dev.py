1b2e7c426b7f3c2c5bfb2bcbc89bb1c0ba8d5d15
Sidharth Sikka <sidharthsikka@users.noreply.github.com>
2016-11-03 15:42:51 +0000

    Update dev.py

--- groupo/settings/dev.py
+++ groupo/settings/dev.py
@@ -1,1 +1,21 @@
 from groupo.settings.common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1:8000']
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
